3980999845305deed97e0bce251a3cb264ae62b8
NISR <nisr@local.dev>
2026-03-02 10:09:37 +0200

    commit..

--- laravel-app/bootstrap/app.php
+++ laravel-app/bootstrap/app.php
@@ -13,10 +13,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->web(remove: [
-            \Illuminate\Cookie\Middleware\EncryptCookies::class,
-        ]);
-
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
         ]);
